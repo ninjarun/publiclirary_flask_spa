a22a8edd89ed17c08f72385796656b41ff68ce54
yoni <orenyoni87@gmail.com>
2022-11-29 22:46:45 +0200

    cors!

--- publiclibrary/__init__.py
+++ publiclibrary/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r'/api/*':{'origins':'*'}})
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db_library.sqlite3'
 app.config['SECRET_KEY'] = "random_string"
 
